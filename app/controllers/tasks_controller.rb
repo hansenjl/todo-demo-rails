a1bb198ef5f368672ae9720778ec657031a46323
hansenjl <jenn.leigh.hansen@gmail.com>
2018-04-03 23:10:48 -0700

    Set up basic app structure

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,30 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    render json: @tasks
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render json: @task
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render json: @task
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      render json: @task
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :completed)
+  end
+end
